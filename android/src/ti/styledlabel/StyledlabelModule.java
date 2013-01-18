d757d0826e25d2e8f6273240392b36bdde0aa662
Jon Alter <jon@local>
2013-01-18 10:20:42 -0800

    [TIMODOPEN-213] Fixing Copyright in Source

--- android/src/ti/styledlabel/StyledlabelModule.java
+++ android/src/ti/styledlabel/StyledlabelModule.java
@@ -1,7 +1,6 @@
 /**
  * Ti.StyledLabel Module
  * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
 
